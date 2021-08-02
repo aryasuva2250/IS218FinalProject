6904b4b9c626de5fd1f5b2f47518a23d37cd00a8
aryasuva2250 <as3526@njit.edu>
2021-08-02 08:55:28 -0400

    Step 4 from tutorial

--- app/app.py
+++ app/app.py
@@ -1,14 +1,16 @@
-from flask import Flask, make_response, request, jsonify
+from flask import Flask, render_template
 
 
-# Create Flask's `app` object
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    template_folder="templates"
+)
 
-
-@app.route("/", methods=['GET'])
-def hello():
-    if request.method != 'GET':
-        return make_response('Malformed request', 400)
-    my_dict = {'key': 'dictionary value'}
-    headers = {"Content-Type": "application/json"}
-    return make_response(jsonify(my_dict), 200, headers)
+@app.route("/")
+def home():
+    """Serve homepage template."""
+    return render_template(
+        'index.html',
+        title='Flask-Login Tutorial.',
+        body="You are now logged in!"
+    )
